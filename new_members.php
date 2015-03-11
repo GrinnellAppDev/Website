f8a97841b3dc15c6015f2f992a3710b74d81ade7
Sarah Trop <sct14112@comcast.net>
2015-03-10 20:43:32 -0500

    css and table update

--- new_members.php
+++ new_members.php
@@ -21,93 +21,93 @@ include('inc/header.php');
 <!-- LEADS -->
 	  		<div class="row">
 	  	<!-- LEA -->
-	   	 		<div class="col-md-12">
-	   	 			<img src="img/lea.png" class="paletteLead"/>
+	   	 		<div class="col-md-2">
+	   	 			<img src="img/lea.png" class="palette"/>
 	   	 		</div>
 	   	 <!-- PATRICK -->
-	   	 		<div class="col-md-12">
-	   	 			<img src="img/patrick.png" class="paletteLead"/>
+	   	 		<div class="col-md-2">
+	   	 			<img src="img/patrick.png" class="palette"/>
 	   	 		</div>
 	   	 <!-- CALEB -->
-	   	 		<div class="col-md-12">
-	   	 			<img src="img/caleb.png" class="paletteLead"/>
+	   	 		<div class="col-md-2">
+	   	 			<img src="img/caleb.png" class="palette"/>
 	   	 		</div>
 	   	 <!-- NATHALIE -->
-	   	 		<div class="col-md-12">
-	   	 			<img src="img/nathalie.png" class="paletteLead"/>
+	   	 		<div class="col-md-2">
+	   	 			<img src="img/nathalie.png" class="palette"/>
 	   	 		</div>
 	   	 <!-- LARRY -->
-	   	 		<div class="col-md-12">
-	   	 			<img src="img/larry.png" class="paletteLead"/>
+	   	 		<div class="col-md-2">
+	   	 			<img src="img/larry.png" class="palette"/>
 	   	 		</div>
 	  		</div>
 	  		<div class="row">
 	  	<!-- ALEX -->
-	   	 		<div class="col-md-12">
-	   	 			<img src="img/alex.png" class="paletteLead"/>
+	   	 		<div class="col-md-2">
+	   	 			<img src="img/alex.png" class="palette"/>
 	   	 		</div>
 	   	 <!-- SAMMEE -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/samee.png" class="paletteLead"/>
 	   	 		</div>
 	   	 <!-- AJUNA -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/ajuna.png" class="paletteLead"/>
 	   	 		</div>
 
 	 <!-- DEVELOPERS -->
 	 	<!-- DANIEL T -->
-	    		<div class="col-md-12">
+	    		<div class="col-md-2">
 	    			<img src="img/danielT.png" class="paletteDeveloper"/>
 	    		</div>
 	    <!-- MICHAEL -->
-	    		<div class="col-md-12">
+	    		<div class="col-md-2">
 	    			<img src="img/michael.png" class="paletteDeveloper"/>
 	    		</div>
 	  		</div>
 	  		<div class="row">
 	  	<!-- TY -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/ty.png" class="paletteDeveloper"/>
 	   	 		</div>
 	   	 <!-- PATRICK S -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/patrickS.png" class="paletteDeveloper"/>
 	   	 		</div>
 	   	 <!-- TIFFANY -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/tiffany.png" class="paletteDeveloper"/>
 	   	 		</div>
 	   	 <!-- DANIEL G -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/danielG.png" class="paletteDeveloper"/>
 	   	 		</div>
 	   	 <!-- AMANDA -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/defaultPerson.png" class="paletteDeveloper"/>
 	   	 		</div>
 	  		</div>
 	  		<div class="row">
 	  	<!-- SHAUN -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/shaun.png" class="paletteDeveloper"/>
 	   	 		</div>
 
 	 <!-- UX & RESEARCH -->
 		<!-- SARAH -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/sarah.png" class="paletteUX"/>
 	   	 		</div>
 	   	 <!-- LEXY -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/lexy.png" class="paletteUX"/>
 	   	 		</div>
 	   	 <!-- HANNAH -->
-	    		<div class="col-md-12">
+	    		<div class="col-md-2">
 	    			<img src="img/hannah.png" class="paletteUX"/>
 	    		</div>
 	    <!-- CHRIS -->
-	    		<div class="col-md-12">
+	    		<div class="col-md-2">
 	    			<img src="img/chris.png" class="paletteUX"/>
 	    		</div>
 	  		</div>
@@ -115,43 +115,43 @@ include('inc/header.php');
 	 <!-- COMMUNITY -->
 	  		<div class="row">
 	  	<!-- MARK -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/mark.png" class="paletteCommunity"/>
 	   	 		</div>
 	   	 <!-- JARREN -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/jarren.png" class="paletteCommunity"/>
 	   	 		</div>
 	   	 <!-- CHLOE -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/defaultPerson.png" class="paletteCommunity"/>
 	   	 		</div>
 
 	<!-- PAST -->
 		<!-- CHRISTINE -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/christine.png" class="palettePast"/>
 	   	 		</div>
 	   	 <!-- SPENCER -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/spencer.png" class="palettePast"/>
 	   	 		</div>
 	  		</div>
 	  		<div class="row">
 	  	<!-- COLIN -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/defaultPerson.png" class="palettePast"/>
 	   	 		</div>
 	   	<!-- MAIJID -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/maijid.png" class="palettePast"/>
 	   	 		</div>
 	   	 <!-- DUGAN -->
-	   	 		<div class="col-md-12">
+	   	 		<div class="col-md-2">
 	   	 			<img src="img/defaultPerson.png" class="palettePast"/>
 	   	 		</div>
 	   	 <!-- DAVID -->
-	    		<div class="col-md-12">
+	    		<div class="col-md-2">
 	    			<img src="img/david.png" class="palettePast"/>
 	    		</div>
 	  		</div>
