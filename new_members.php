0301db1b4dd1d361f3d63fb4482d75e9aa362d88
Sarah Trop <sct14112@comcast.net>
2015-03-10 21:13:11 -0500

    resetting

--- new_members.php
+++ new_members.php
@@ -16,27 +16,26 @@ include('inc/header.php');
 
 	<div class="container membersPalette">
 		<div style="paletteStyle">
-			<div class="col-md-4">
-			</div>
+			<center>
 		<!-- LEADS -->
 			  		<div class="row">
 			  	<!-- LEA -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/lea.png" class="palette paletteLead"/>
 			   	 		</div>
 
 			   	 <!-- PATRICK -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/patrick.png" class="palette paletteLead"/>
 			   	 		</div>
 
 			   	 <!-- CALEB -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/caleb.png" class="palette paletteLead"/>
 			   	 		</div>
 
 			   	 <!-- NATHALIE -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/nathalie.png" class="palette paletteLead"/>
 			   	 		</div>
 			   	 	</div>
@@ -44,21 +43,21 @@ include('inc/header.php');
 			  		<div class="row">
 
 			  	<!-- LARRY -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/larry.png" class="palette paletteLead"/>
 			   	 		</div>
 			  	<!-- ALEX -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/alex.png" class="palette paletteLead"/>
 			   	 		</div>
 
 			   	 <!-- SAMMEE -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/samee.png" class="palette paletteLead"/>
 			   	 		</div>
 
 			   	 <!-- AJUNA -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/ajuna.png" class="palette paletteLead"/>
 			   	 		</div>
 			   	 	</div>
@@ -67,22 +66,22 @@ include('inc/header.php');
 			 		<div class="row">
 
 			 	<!-- DANIEL T -->
-			    		<div class="col-md-12">
+			    		<div class="col-md-2">
 			    			<img src="img/danielT.png" class="palette paletteDeveloper"/>
 			    		</div>
 
 			    <!-- MICHAEL -->
-			    		<div class="col-md-12">
+			    		<div class="col-md-2">
 			    			<img src="img/michael.png" class="palette paletteDeveloper"/>
 			    		</div>
 			
 			  	<!-- TY -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/ty.png" class="palette paletteDeveloper"/>
 			   	 		</div>
 
 			   	 <!-- PATRICK S -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/patrickS.png" class="palette paletteDeveloper"/>
 			   	 		</div>
 			   	 	</div>
@@ -90,22 +89,22 @@ include('inc/header.php');
 					<div class="row">
 
 			   	 <!-- TIFFANY -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/tiffany.png" class="palette paletteDeveloper"/>
 			   	 		</div>
 			   	 <!-- DANIEL G -->
 
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/danielG.png" class="palette paletteDeveloper"/>
 			   	 		</div>
 
 			   	 <!-- AMANDA -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/defaultPerson.png" class="palette paletteDeveloper"/>
 			   	 		</div>
 
 			  	<!-- SHAUN -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/shaun.png" class="palette paletteDeveloper"/>
 			   	 		</div>
 			   	 	</div>
@@ -114,22 +113,22 @@ include('inc/header.php');
 
 			 <!-- UX & RESEARCH -->
 				<!-- SARAH -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/sarah.png" class="palette paletteUX"/>
 			   	 		</div>
 
 			   	 <!-- LEXY -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/lexy.png" class="palette paletteUX"/>
 			   	 		</div>
 
 			   	 <!-- HANNAH -->
-			    		<div class="col-md-12">
+			    		<div class="col-md-2">
 			    			<img src="img/hannah.png" class="palette paletteUX"/>
 			    		</div>
 
 			    <!-- CHRIS -->
-			    		<div class="col-md-12">
+			    		<div class="col-md-2">
 			    			<img src="img/chris.png" class="palette paletteUX"/>
 			    		</div>
 			  		</div>
@@ -138,23 +137,23 @@ include('inc/header.php');
 			  		<div class="row">
 
 			  	<!-- MARK -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/mark.png" class="palette paletteCommunity"/>
 			   	 		</div>
 
 			   	 <!-- JARREN -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/jarren.png" class="palette paletteCommunity"/>
 			   	 		</div>
 
 			   	 <!-- CHLOE -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/defaultPerson.png" class="palette paletteCommunity"/>
 			   	 		</div>
 
 			<!-- PAST -->
 				<!-- CHRISTINE -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/christine.png" class="palette palettePast"/>
 			   	 		</div>
 			   	 	</div>
@@ -162,22 +161,22 @@ include('inc/header.php');
 			   	 	<div class="row">
 
 			   	 <!-- SPENCER -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/spencer.png" class="palette palettePast"/>
 			   	 		</div>
 
 			  	<!-- COLIN -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/defaultPerson.png" class="palette palettePast"/>
 			   	 		</div>
 
 			   	<!-- MAIJID -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/maijid.png" class="palette palettePast"/>
 			   	 		</div>
 
 			   	 <!-- DUGAN -->
-			   	 		<div class="col-md-12">
+			   	 		<div class="col-md-2">
 			   	 			<img src="img/defaultPerson.png" class="palette palettePast"/>
 			   	 		</div>
 			   	 	</div>
@@ -185,14 +184,12 @@ include('inc/header.php');
 			   	 	<div class="row">
 
 			   	 <!-- DAVID -->
-			    		<div class="col-md-12">
+			    		<div class="col-md-2">
 			    			<img src="img/david.png" class="palette palettePast"/>
 			    		</div>
 			  		</div>
 
-  			<div class="col-md-4">
-			</div>
-
+  			</center>
   		</div>
   	</div>
 </div>
