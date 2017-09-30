ffa7774285f98965f44f403f9b4f1cb7ba378c62
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-09-30 00:16:46 -0500

    Update webhook to give better feedback.

--- github-webhook.php
+++ github-webhook.php
@@ -2,19 +2,34 @@
     header("Content-Type: application/json");
 
     $request_signature = $_SERVER["HTTP_X_HUB_SIGNATURE"];
-    if ($request_signature) {
 
+    if ($request_signature) {
         $secret = getenv("GITHUB_WEBHOOK_SECRET");
         $request_body = file_get_contents("php://input");
         $body_hash = "sha1=" . hash_hmac("sha1", $request_body, $secret);
 
         if ($body_hash === $request_signature) {
             $event = $_SERVER["HTTP_X_GITHUB_EVENT"];
+
             if ($event === "push") {
-                $output = shell_exec("git pull origin master");
-                echo json_encode(array("text" => $output), JSON_PRETTY_PRINT);
-                echo "\n";
-                http_response_code(200);
+                exec("git pull origin master", $output, $shell_return);
+
+                if (!$shell_return) {
+                    echo json_encode(array(
+                        "message" => implode("\n", $output),
+                        "lines" => $output
+                    ), JSON_PRETTY_PRINT);
+                    echo "\n";
+                    http_response_code(200);
+                } else {
+                    echo json_encode(array(
+                        "error" => "pull_fail",
+                        "message" => implode("\n", $output),
+                        "return_code" => $shell_return
+                    ), JSON_PRETTY_PRINT);
+                    echo "\n";
+                    http_response_code(500);
+                }
             } else if ($event === "ping") {
                 http_response_code(204);
             } else {
