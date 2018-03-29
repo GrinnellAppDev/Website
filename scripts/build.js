c7a9694e70aab386910d03a9a96d02a7333e47a0
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-03-29 01:23:42 -0500

    Clean up build script

--- scripts/build.js
+++ scripts/build.js
@@ -2,55 +2,67 @@ const fs = require("fs-extra")
 const mustache = require("mustache")
 const path = require("path")
 
-const root = `${__dirname}/..`
+const build = () => {
+    const rootDir = `${__dirname}/..`
+    const publicDir = `${rootDir}/src/public`
+    const dataDir = `${rootDir}/src/data`
+    const templatesDir = `${rootDir}/src/templates`
+    const wwwDir = `${rootDir}/www`
 
-// Purge old build
-fs.ensureDirSync(`${root}/www`)
-const oldFiles = fs.readdirSync(`${root}/www`)
-oldFiles.forEach(file => {
-    fs.removeSync(`${root}/www/${file}`)
-})
-
-// Copy over contents of public
-const publicFiles = fs.readdirSync(`${root}/src/public`)
-publicFiles.forEach(file => {
-    fs.copySync(`${root}/src/public/${file}`, `${root}/www/${file}`)
-})
-
-// Copy over contents of data
-const dataFiles = fs.readdirSync(`${root}/src/data`)
-dataFiles.forEach(file => {
-    fs.copySync(`${root}/src/data/${file}`, `${root}/www/${file}`)
-})
-
-// Render templates
-
-const templateFiles = fs.readdirSync(`${root}/src/templates`)
-const partialFiles = templateFiles.filter(fileName => fileName[0] === "_")
-
-const partials = {}
-partialFiles.forEach(file => {
-    const fileName = path.basename(file, path.extname(file))
-
-    // Strip out the leading "_"
-    const cleanFileName = fileName.substr(1)
-
-    partials[cleanFileName] = fs.readFileSync(`${root}/src/templates/${file}`, {
-        encoding: "utf-8"
+    // Purge old build
+    fs.ensureDirSync(wwwDir)
+    const oldFiles = fs.readdirSync(wwwDir)
+    oldFiles.forEach(file => {
+        fs.removeSync(`${wwwDir}/${file}`)
     })
-})
 
-templateFiles.filter(fileName => fileName[0] !== "_").forEach(file => {
-    const fileName = path.basename(file, path.extname(file))
+    // Copy over contents of public
+    const publicFiles = fs.readdirSync(publicDir)
+    publicFiles.forEach(file => {
+        fs.copySync(`${publicDir}/${file}`, `${wwwDir}/${file}`)
+    })
 
-    fs.writeFileSync(
-        `${root}/www/${file}`,
-        mustache.render(
-            fs.readFileSync(`${root}/src/templates/${file}`, {
-                encoding: "utf-8"
-            }),
-            { page: fileName },
-            partials
+    // Copy over contents of data
+    const dataFiles = fs.readdirSync(dataDir)
+    dataFiles.forEach(file => {
+        fs.copySync(`${dataDir}/${file}`, `${wwwDir}/${file}`)
+    })
+
+    // Render templates
+
+    const templateFiles = fs.readdirSync(templatesDir)
+    const partialFiles = templateFiles.filter(fileName => fileName[0] === "_")
+
+    const partials = {}
+    partialFiles.forEach(file => {
+        const fileName = path.basename(file, path.extname(file))
+
+        // Strip out the leading "_"
+        const cleanFileName = fileName.substr(1)
+
+        partials[cleanFileName] = fs.readFileSync(`${templatesDir}/${file}`, {
+            encoding: "utf-8"
+        })
+    })
+
+    templateFiles.filter(fileName => fileName[0] !== "_").forEach(file => {
+        const fileName = path.basename(file, path.extname(file))
+
+        fs.writeFileSync(
+            `${wwwDir}/${file}`,
+            mustache.render(
+                fs.readFileSync(`${templatesDir}/${file}`, {
+                    encoding: "utf-8"
+                }),
+                { page: fileName },
+                partials
+            )
         )
-    )
-})
+    })
+}
+
+if (module === require.main) {
+    build()
+} else {
+    module.exports = build
+}
